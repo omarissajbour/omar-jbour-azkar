8ace52d9fdfe42136a8fe319501cc95d71a19d5a
omarissajbour <221148@ppu.edu.ps>
2025-07-13 23:11:56 +0300

    some update

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,34 @@ import "./App.css";
 
 function App() {
   const [cards, setCards] = useState([]);
-  <h1>omar jbour id : 221148</h1>
 
   useEffect(() => {
     fetch("/azkar.json")
       .then((res) => res.json())
       .then((data) => {
-        setCards(data.cards.slice(0, 30)); // نعرض أول 6 أذكار فقط
+        setCards(data.cards.slice(0, 30)); // نعرض أول 30 ذكر فقط
       });
   }, []);
 
   return (
-    <div className="cards-container">
-      {cards.map((card) => (
-        <Counter
-          key={card.id}
-          title={card.title}
-          numoftries={card.count}
-          backcolor={card.backgroundColor}
-        />
-      ))}
+    <div>
+      <header className="main-header">
+        <h1>أذكار المسلم</h1>
+        <p className="subtitle">omar jbour — ID: 221148</p>
+      </header>
+
+      <div className="cards-container">
+        {cards.map((card) => (
+          <Counter
+            key={card.id}
+            title={card.title}
+            numoftries={card.count}
+            backcolor={card.backgroundColor}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
 
 export default App;
